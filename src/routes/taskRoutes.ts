8630d042a3ae45731143621ac22b40e85d23e430
bitsen03 <bitsen03@mail.ru>
2025-03-06 14:51:30 +0300

    feat: переделал запросы с axios на fetch, перенес логику в функцию task, удалил axios jest

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,118 +1,113 @@
-import { Router, Request, Response } from 'express';
-import axios from 'axios';
-
-const router = Router();
 const domainName = 'domain_name'; 
 const accessToken = 'access_token'; 
 const webhookDestination = 'http://webhooks.avantiiva.com/ff9ef597-cccc-42ee-ffff-52cb55a04f0e';
 const webhookSettings = ["note_lead","status_lead","note_contact"]
 
-router.get('/checkStatus', async (req: Request, res: Response) => {
-  const url = `https://${domainName}.amocrm.ru/api/v4/webhooks?page=0`;
 
-  try {
-    // const response = await axios.get(url, {
-    //   headers: {
-    //     Authorization: `Bearer ${accessToken}`,
-    //   },
-    // });
+export async function checkStatus() {
+    const url = `https://${domainName}.amocrm.ru/api/v4/webhooks?page=0`;
 
-    /*
-    const webhooks = response.data._embedded.webhooks;
-    const targetWebhook = webhooks.find((hook: any) => hook.destination === webhookDestination);
-    */
-
-    // Пример мока ответа
-    const mockResponse = {
-        "_total_items": 3,
-        "_embedded": {
-            "webhooks": [{
-                    "id": 5044731,
-                    "destination": "https://api.roydis.tech/hooks/amocrm/tasks_responsible",
-                    "created_at": 1680591792,
-                    "updated_at": 1730626214,
-                    "account_id": 29005714,
-                    "created_by": 7542007,
-                    "sort": 1,
-                    "disabled": false,
-                    "settings": ["responsible_lead", "responsible_company", "responsible_contact"]
-                }, {
-                    "id": 62783,
-                    "destination": "https://umnico.com/api/webhooks/amocrm2",
-                    "created_at": 1686082289,
-                    "updated_at": 1738752110,
-                    "account_id": 29005714,
-                    "created_by": 6254656,
-                    "sort": 1,
-                    "disabled": false,
-                    "settings": ["status_lead", "add_unsorted"]
-                }, {
-                    "id": 43022,
-                    "destination": "http://webhooks.avantiiva.com/ff9ef597-cccc-42ee-ffff-52cb55a04f0e",
-                    "created_at": 1737129421,
-                    "updated_at": 1737129421,
-                    "account_id": 29005714,
-                    "created_by": 6254656,
-                    "sort": 1,
-                    "disabled": true, // тест
-                    "settings": ["note_lead", "status_lead", "note_contact"]
-                }
-            ]
-        }
-    };
-    
-    
-
-    const webhooks = mockResponse._embedded.webhooks;
-    const targetWebhook = webhooks.find((hook: any) => hook.destination === webhookDestination);
-
-    if (targetWebhook && targetWebhook.disabled) {
-      await deleteWebhook();
-      await createWebhook();
-      res.status(200).json({ message: 'Webhook updated' });
-    } else {
-      res.status(200).json(mockResponse);
+    try {
+      // const response = await fetch(url, {
+      //     method: 'GET',
+      //     headers: {
+      //       Authorization: `Bearer ${accessToken}`,
+      //     },
+      // });
+  
+      /*
+      const webhooks = response.data._embedded.webhooks;
+      const targetWebhook = webhooks.find((hook: any) => hook.destination === webhookDestination);
+      */
+  
+      // Пример мока ответа
+      const mockResponse = {
+          "_total_items": 3,
+          "_embedded": {
+              "webhooks": [{
+                      "id": 5044731,
+                      "destination": "https://api.roydis.tech/hooks/amocrm/tasks_responsible",
+                      "created_at": 1680591792,
+                      "updated_at": 1730626214,
+                      "account_id": 29005714,
+                      "created_by": 7542007,
+                      "sort": 1,
+                      "disabled": false,
+                      "settings": ["responsible_lead", "responsible_company", "responsible_contact"]
+                  }, {
+                      "id": 62783,
+                      "destination": "https://umnico.com/api/webhooks/amocrm2",
+                      "created_at": 1686082289,
+                      "updated_at": 1738752110,
+                      "account_id": 29005714,
+                      "created_by": 6254656,
+                      "sort": 1,
+                      "disabled": false,
+                      "settings": ["status_lead", "add_unsorted"]
+                  }, {
+                      "id": 43022,
+                      "destination": "http://webhooks.avantiiva.com/ff9ef597-cccc-42ee-ffff-52cb55a04f0e",
+                      "created_at": 1737129421,
+                      "updated_at": 1737129421,
+                      "account_id": 29005714,
+                      "created_by": 6254656,
+                      "sort": 1,
+                      "disabled": true, // тест
+                      "settings": ["note_lead", "status_lead", "note_contact"]
+                  }
+              ]
+          }
+      };
+      
+      
+  
+      const webhooks = mockResponse._embedded.webhooks;
+      const targetWebhook = webhooks.find((hook: any) => hook.destination === webhookDestination);
+  
+      return (targetWebhook && targetWebhook.disabled);
+    } catch (err) {
+      console.error(err);
+      //response.status(500).json({ message: 'Error fetching webhooks', err });
     }
-  } catch (err: any) {
-    console.error(err);
-    res.status(500).json({ message: 'Error fetching webhooks', err });
-  }
-});
+}
 
-async function deleteWebhook() {
+export async function deleteWebhook() {
   const url = `https://${domainName}.amocrm.ru/api/v4/webhooks`;
   try {
     /*
-    await axios.delete(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      },
-      data: {
-        destination: webhookDestination,
-      },
-    });
+        await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            destination: webhookDestination,
+        }),
+        });
     */
     
     console.log('Webhook deleted (mock)');
-  } catch (err: any) {
+  } catch (err) {
     console.error('Error deleting webhook:', err);
   }
 }
 
-async function createWebhook() {
+export async function createWebhook() {
   const url = `https://${domainName}.amocrm.ru/api/v4/webhooks`;
   try {
     /*
-    await axios.post(url, {
-      destination: webhookDestination,
-      settings: webhookSettings,
-    }, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    });
+        await fetch(url, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            destination: webhookDestination,
+            settings: webhookSettings,
+        }),
+        });
     */
     
     console.log('Webhook created (mock)');
@@ -120,5 +115,3 @@ async function createWebhook() {
     console.error('Error creating webhook:', err);
   }
 }
-
-export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,29 @@
 import express, {Request, Response} from 'express';
 import cron from 'node-cron'
-import taskRouter from './routes/taskRoutes';
+import { checkStatus, deleteWebhook, createWebhook } from './routes/taskRoutes';
 
 const app = express();
 const port = 3075;
 
 let statusTask: boolean = false;
 
-const fakeWork = async ():Promise<void> => {
-  try {
-    setTimeout(() => {
-      statusTask = false;
-      console.log('task1 не работает')
-    }, 3000);
-  } catch(err) {
-    throw new Error(`Ошибка выполнения fakeWork: ${err}`);
-  }
-}
-
 const task = async ():Promise<void> => {
   if (!statusTask){
     statusTask = true;
     console.log('task запустился работает');
-    await fakeWork();
+    const status = await checkStatus();
+    
+    if (status) {
+      await deleteWebhook();
+      await createWebhook();
+    }
+
   } else {
     console.log('task уже запущен');
   }
 }
 
-cron.schedule('* * * * * *', () => task());
+cron.schedule('* * * * *', () => task());
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('Приложения работает!');
@@ -37,6 +32,3 @@ app.get('/', (req: Request, res: Response): void => {
 app.listen(port, (): void => {
   console.log(`Пример приложения прослушивает порт ${port}`);
 });
-
-app.use('/task', taskRouter)
-
